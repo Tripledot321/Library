0765ea9ea0168b3163bdf56955797f201f9f5deb
Tripledot321 <96250388+Tripledot321@users.noreply.github.com>
2022-04-18 21:27:12 +0200

    GUI

--- src/GUI.java
+++ src/GUI.java
@@ -1,21 +1,33 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class GUI {
 
+    JFrame frame;
+    JPanel panel;
+    JPanel titleNamePanel;
+    JLabel label;
+    Container container;
+
     public GUI() {
 
-        JFrame frame = new JFrame();
+        frame = new JFrame();
 
-        JPanel panel = new JPanel();
-        panel.setBorder(BorderFactory.createEmptyBorder(30, 30, 10 ,30));
-        panel.setLayout(new GridLayout(0, 1));
-
-        frame.add(panel, BorderLayout.CENTER);
+        frame.setSize(1200, 800);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setTitle("LTU Library");
-        frame.pack();
         frame.setVisible(true);
+        frame.setLayout(null);
+        container = frame.getContentPane();
+
+        titleNamePanel = new JPanel();
+        titleNamePanel.setBounds(100, 100, 1000, 150);
+        titleNamePanel.setBackground(Color.black);
+        
+        container.add(titleNamePanel);
 
     }
+
 }
