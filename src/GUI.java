0603cff849c544e0dac4f18759a3fd256d7c9624
Tripledot321 <96250388+Tripledot321@users.noreply.github.com>
2022-04-23 21:55:44 +0200

    123

--- src/GUI.java
+++ src/GUI.java
@@ -1,33 +1,48 @@
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
 public class GUI {
 
     JFrame frame;
-    JPanel panel;
-    JPanel titleNamePanel;
-    JLabel label;
-    Container container;
-    Font normalfont = new Font("Times New Roman", Font.plain, 26);
+    JPanel titleNamePanel, startButtonPanel;
+    JLabel titleNameLabel;
+    JButton startButton;
+    Font bigFont = new Font("Times New Roman", Font.PLAIN, 90);
+    Font normalFont = new Font("Times New Roman", Font.PLAIN, 26);
 
     public GUI() {
 
+        // Window
         frame = new JFrame();
-
         frame.setSize(1200, 800);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setTitle("LTU Library");
-        frame.setVisible(true);
+        frame.getContentPane().setBackground(Color.white);
+        frame.setTitle("Library");
         frame.setLayout(null);
-        container = frame.getContentPane();
 
+        // Title Screen
         titleNamePanel = new JPanel();
         titleNamePanel.setBounds(100, 100, 1000, 150);
         titleNamePanel.setBackground(Color.black);
+        titleNameLabel = new JLabel("LTU Library");
+        titleNameLabel.setForeground(Color.white);
+        titleNameLabel.setFont(bigFont);
+        titleNamePanel.add(titleNameLabel);
 
-        container.add(titleNamePanel);
+        startButtonPanel = new JPanel();
+        startButtonPanel.setBounds(500, 400, 200, 100);
+        startButtonPanel.setBackground(Color.black);
+        startButton = new JButton("ENTER");
+        startButton.setBackground(Color.black);
+        startButton.setForeground(Color.white);
+        startButton.setFont(normalFont);
+        startButton.setFocusPainted(false);
+        startButtonPanel.add(startButton);
+
+        frame.add(titleNamePanel);
+        frame.add(startButtonPanel);
+
+        frame.setVisible(true);
 
     }
 
