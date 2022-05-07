89bfccccbf1981144d2da2f2b5bbfee119530da3
leavingw <mellstroemjohan@gmail.com>
2022-05-07 14:29:12 +0200

    Changed Layout in Frame to have Header at the top and Main panel below it

--- src/Package1/GUI.java
+++ src/Package1/GUI.java
@@ -18,8 +18,10 @@ public class GUI {
         frame.setSize(1200, 800);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.getContentPane().setBackground(Color.white);
+        frame.setLayout(new BorderLayout());
         frame.setTitle("Library");
-//        frame.setLayout(null);
+        frame.setVisible(true);
+
 
 //        JPanel panel = new JPanel();
 //        panel.setLayout(new FlowLayout(FlowLayout.CENTER, 10, 5));
@@ -30,23 +32,33 @@ public class GUI {
 //        panel.setVisible(true);
 //
 //        frame.add(panel, BorderLayout.NORTH);
-//        frame.setVisible(true);
+
 
         // headerPanel
         headerPanel = new JPanel();
-        headerPanel.setBounds(0, 0, 1200, 120);
+//        headerPanel.setBounds(0, 0, 1200, 120);
         headerPanel.setBackground(Color.black);
         headerPanel.setLayout(new FlowLayout(FlowLayout.CENTER, 10, 5));
-        frame.add(headerPanel);
+        frame.add(headerPanel, BorderLayout.NORTH);
+
+//        JPanel panel = new JPanel();
+//        panel.setLayout(new FlowLayout(FlowLayout.CENTER, 10, 5));
+//        panel.setBackground(Color.RED);
+//
+//        Button button = new Button("Button");
+//        panel.add(button);
+//        panel.setVisible(true);
+
+//        headerPanel.add(panel);
+
 
 
         // Package1.Main Menu
         mainPanel = new JPanel();
-        mainPanel.setBounds(100, 130, 900, 600);
-        mainPanel.setBackground(Color.white);
+//        mainPanel.setBounds(100, 130, 900, 600);
+        mainPanel.setBackground(Color.red);
         mainPanel.setLayout(new FlowLayout(FlowLayout.CENTER, 10, 5));
-        frame.add(mainPanel);
-
+        frame.add(mainPanel, BorderLayout.CENTER);
 
         //lägg till saker i headerPanel
         JLabel titleNameLabel = new JLabel("LTU Library");
@@ -56,7 +68,7 @@ public class GUI {
 
         JButton loginButton = new JButton("Login");
         //Det är nåt som inte blir bra när man försöker flytta saker, oavsett vilket x-värde så samma plats.. :'(
-        loginButton.setBounds(1200, 30,300, 50);
+//        loginButton.setBounds(1200, 30,300, 50);
         //loginButton.setLocation(1100,30);
         loginButton.setFont(normalFont);
         loginButton.setBackground(Color.WHITE);
@@ -68,7 +80,7 @@ public class GUI {
         //
         JButton goToSearchButton = new JButton("Search");
         //Det är nåt som inte blir bra när man försöker flytta saker, oavsett vilket x-värde så samma plats.. :'(
-        goToSearchButton.setBounds(1200, 100,300, 50);
+//        goToSearchButton.setBounds(1200, 100,300, 50);
         goToSearchButton.setFont(normalFont);
         goToSearchButton.setBackground(Color.WHITE);
         goToSearchButton.setFocusPainted(false);
@@ -79,9 +91,19 @@ public class GUI {
 
         //Search button
         searchBooksPanel = new JPanel();
-        searchBooksPanel.setBounds(500, 350, 200, 100);
+//        searchBooksPanel.setBounds(500, 350, 1000, 100);
+        searchBooksPanel.setPreferredSize(searchBooksPanel.getPreferredSize());
         searchBooksPanel.setBackground(Color.white);
+        searchBooksPanel.setLayout(new FlowLayout(FlowLayout.LEADING, 10, 5));
         mainPanel.add(searchBooksPanel);
+
+        //search fields with labels
+
+        //search field
+        JTextField searchField = new JTextField();
+        searchField.setPreferredSize(new Dimension(550,30));
+        searchField.setBackground(Color.lightGray);
+        searchBooksPanel.add(searchField);
 
         JButton searchButton = new JButton("Search");
         searchButton.setBackground(Color.black);
@@ -96,24 +118,21 @@ public class GUI {
 
 
 
-        //search fields with labels
 
-        //search field
-        JTextField searchField = new JTextField();
-        searchField.setPreferredSize(new Dimension(550,30));
-        searchField.setBackground(Color.lightGray);
-        searchBooksPanel.add(searchField);
 
 
         //Login screen username
 
         //username text field
         loginPanel = new JPanel();
+        loginPanel.setLayout(new FlowLayout(FlowLayout.CENTER, 10, 5));
+
         mainPanel.add(loginPanel);
 
         //usernamePanel.setBounds(300, 300, 200, 30);
         //usernamePanel.setBackground(Color.white);
         JTextField usernameField = new JTextField();
+        usernameField.setText("Please enter your username and password");
         //usernameField.setPreferredSize(new Dimension(200,30));
         usernameField.setBackground(Color.lightGray);
         loginPanel.add(usernameField);
@@ -143,61 +162,6 @@ public class GUI {
         //sen när man vill att dom ska synas ändrar man i metoden i VisibilityManager
         loginScreenLabel.setVisible(false);
         loginPanel.add(loginScreenLabel);
-
-
-
-
-
-
-
-
-        JPanel choiceButtonPanel = new JPanel();
-        choiceButtonPanel.setBounds(250, 600, 475, 150);
-
-
-        //Tror dessa är överflödiga, det är knappar som vi inte lagt någonstans, men såg att dom var med i videon
-      /*  choice1 = new JButton("choice1");
-        choice1.setBackground(Color.black);
-        choice1.setForeground(Color.white);
-        choice1.setFont(normalFont);
-        choice1.setFocusPainted(false);
-        choice1.addActionListener(cHandler);
-        choice1.setActionCommand("c1");
-        choiceButtonPanel.add(choice1);
-
-        choice2 = new JButton("choice2");
-        choice2.setBackground(Color.black);
-        choice2.setForeground(Color.white);
-        choice2.setFont(normalFont);
-        choice2.setFocusPainted(false);
-        choice2.addActionListener(cHandler);
-        choice2.setActionCommand("c2");
-        choiceButtonPanel.add(choice2);
-
-        choice3 = new JButton("choice3");
-        choice3.setBackground(Color.black);
-        choice3.setForeground(Color.white);
-        choice3.setFont(normalFont);
-        choice3.setFocusPainted(false);
-        choice3.addActionListener(cHandler);
-        choice3.setActionCommand("c3");
-        choiceButtonPanel.add(choice3);
-
-        choice4 = new JButton("choice4");
-        choice4.setBackground(Color.black);
-        choice4.setForeground(Color.white);
-        choice4.setFont(normalFont);
-        choice4.setFocusPainted(false);
-        choice4.addActionListener(cHandler);
-        choice4.setActionCommand("c4");
-        choiceButtonPanel.add(choice4);*/
-
-        frame.add(headerPanel);
-        frame.add(searchBooksPanel);
-        frame.add(mainPanel);
-        frame.add(loginPanel);
-
-        frame.setVisible(true);
 
     }
 
--- src/Package1/VisibilityManager.java
+++ src/Package1/VisibilityManager.java
@@ -5,28 +5,19 @@ public class VisibilityManager {
     GUI gui;
 
     public VisibilityManager(GUI userInterface) {
-
-        //Detta saknades
         gui = userInterface;
         gui.headerPanel.setVisible(true);
         gui.mainPanel.setVisible(true);
-
     }
 
     public void searchResults(){
-
         gui.searchBooksPanel.setVisible(true);
         gui.loginPanel.setVisible(false);
-
     }
 
     public void loginScreen(){
-
-        //insåg nedan att det inte bara är panelerna vi kan styra utan även individuella knappar t.ex.
         gui.searchBooksPanel.setVisible(false);
-
         gui.loginPanel.setVisible(true);
-
     }
 
 }
